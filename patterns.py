43651a89b9fd4eb2ca526736cc4e0e0c0e1ddf1b
Rajshekar Reddy <rajshekarreddy8050@gmail.com>
2026-04-08 21:51:48 +0530

    basic * pattern and number patterns questions

---
+++ patterns.py
@@ -0,0 +1,60 @@
+if __name__ == "__main__":
+    for i in range (1,5+1):
+        print("*")
+
+    print("-"*199)
+
+    #(2)###################
+
+    for i in range(1,6):
+        for j in range(1,6):
+            print("*",end=" ")
+        print()
+
+    print("_"*190)
+
+    ##(3)####################
+
+    for i in range (1,6):
+        for j in range(1,6):
+            print(i,end=" ")
+        print()
+
+    print("-"*100)
+
+    ########################
+
+    for i in range(1,6):
+        for j in range(1,6):
+            print(j,end=" ")
+        print()
+
+    print("__"*100)
+
+    #######################3
+    for i in range(1,5+1):
+        for j in range(1,i+1):
+            print("*",end=" ")
+        print()
+
+    print("_"*190)
+
+    ########################
+
+    for i in range (1,6):
+        for j in range (5,6-i-1,-1):
+            print(j,end=" ")
+        print()
+
+    print("__"*100)
+
+    ############################
+
+    for i in range(1, 6):
+        for j in range(6-i,5+1,1):
+            print(j, end=" ")
+        print()
+
+    print("-"*100)
+    ########################
+
