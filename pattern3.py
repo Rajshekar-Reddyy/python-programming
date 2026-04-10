db10e8f51eaa8bfc003666377a146bd30a7e3f12
Rajshekar Reddy <rajshekarreddy8050@gmail.com>
2026-04-10 23:56:07 +0530

    advanced patterns

---
+++ pattern3.py
@@ -0,0 +1,407 @@
+if __name__ == "__main__":
+    r=5
+    for i in range(1,r+1):
+        for j in range(1,r-i+1):
+            print(" ",end=" ")
+        for j in range(1,i+1):
+            print("*",end=" ")
+        print()
+
+    print("__"*100)
+##############################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(r+1-i,5+1,1):
+            print(j, end=" ")
+        print()
+
+    print("__"*100)
+    #################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(5,r+1-i-1,-1):
+            print(j, end=" ")
+        print()
+
+    print("__" * 100)
+    #################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1,i+1):
+            print(j, end=" ")
+        print()
+
+    print("__" * 100)
+    #################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(i,1-1,-1):
+            print(j, end=" ")
+        print()
+
+    print("__" * 100)
+    ##########################     CHARACTERS PRINTING      #######################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(i, 1 - 1, -1):
+            print(chr(j + 64), end=" ")
+        print()
+
+    print("__"*100)
+    ####################################################################
+    r=5
+    for i in range(1,r+1):
+        for j in range(1,r-i+1):
+            print(" ",end=" ")
+        for j in range(r-i+1,5+1,1):
+            print(chr(j+64),end=" ")
+        print()
+
+    print("__"*100)
+    #######################################################################
+    r=5
+    for i in range(1,r+1):
+        for j in range(1,r-i+1):
+            print(" ",end=" ")
+        for j in range(i,1-1,-1):
+            print(chr(j+64),end=" ")
+        print()
+
+    print("__"*100)
+    #########################################################################
+    r=5
+    for i in range(1,r+1):
+        for j in range(1,r-i+1):
+            print(" ",end=" ")
+        for j in range(5,r+1-i-1,-1):
+            print(chr(j+64),end=" ")
+        print()
+
+    print("_"*100)
+    #####################################################################33
+    r=5
+    for i in range(1,r+1):
+        for j in range(1,r+1-i):
+            print(" ",end=" ")
+        for j in range(1,i+1):
+            print(chr(j+64),end=" ")
+        print()
+
+    print("__"*100)
+   #############################  DAY 6 #####################################
+    r = 5
+    for i in range(1, r + 1):                    ###  TRIANGLE PATTERN
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1, i + 1):
+            print("*", end=" ")
+        for j in range(1,i-1+1):
+            print("*",end=" ")
+        print()
+
+    print("__" * 100)
+    ########################################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(i,1-1,-1):
+            print(j, end=" ")
+        for j in range(2,i+1):
+            print(j, end=" ")
+        print()
+
+    print("__" * 100)
+    ########################################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(5,r+1-i-1,-1):
+            print(j, end=" ")
+        for j in range(r+2-i,r+1):
+            print(j, end=" ")
+        print()
+
+    print("__" * 100)
+    ########################################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(r+1-i,5+1):
+            print(j, end=" ")
+        for j in range(r-1,r+1-i-1,-1):
+            print(j, end=" ")
+        print()
+
+    print("__" * 100)
+    ########################################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1,i+1):
+            print(j, end=" ")
+        for j in range(i-1,1-1,-1):
+            print(j, end=" ")
+        print()
+
+
+    ###############            characters
+
+    print("__" * 100)
+    ########################################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(i, 1 - 1, -1):
+            print(chr(j+64), end=" ")
+        for j in range(2, i + 1):
+            print(chr(j+64), end=" ")
+        print()
+
+    print("__" * 100)
+    ########################################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(5, r + 1 - i - 1, -1):
+            print(chr(j+64), end=" ")
+        for j in range(r + 2 - i, r + 1):
+            print(chr(j+64), end=" ")
+        print()
+
+    print("__" * 100)
+    ########################################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(r + 1 - i, 5 + 1):
+            print(chr(j+64), end=" ")
+        for j in range(r - 1, r + 1 - i - 1, -1):
+            print(chr(j+64), end=" ")
+        print()
+
+    print("__" * 100)
+    ########################################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1, i + 1):
+            print(chr(j+64), end=" ")
+        for j in range(i - 1, 1 - 1, -1):
+            print(chr(j+64), end=" ")
+        print()
+
+    print("__" * 100)
+    print("DIAMOND SHAPE PATTERNS")
+    #####################################       diamond shape patterns         ################
+
+    r = 5
+    for i in range(1, r + 1):  ###  diamond shape
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1, i + 1):
+            print("*", end=" ")
+        for j in range(1, i - 1 + 1):
+            print("*", end=" ")
+        print()
+
+    for i in range(r-1, 1-1,-1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1, i + 1):
+            print("*", end=" ")
+        for j in range(1, i - 1 + 1):
+            print("*", end=" ")
+        print()
+
+    print("__"*100)
+    ##############################################################
+    r = 5
+    for i in range(r,1-1,-1):  ###  diamond shape
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1, i + 1):
+            print("*", end=" ")
+        for j in range(1, i - 1 + 1):
+            print("*", end=" ")
+        print()
+
+    r = 5
+    for i in range(2, r + 1):  ###  diamond shape
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1, i + 1):
+            print("*", end=" ")
+        for j in range(1, i - 1 + 1):
+            print("*", end=" ")
+        print()
+
+    print("__" * 100)
+    ########################################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(r + 1 - i, 5 + 1):
+            print(j, end=" ")
+        for j in range(r - 1, r + 1 - i - 1, -1):
+            print(j, end=" ")
+        print()
+
+    for i in range(r-1, 1-1,-1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(r + 1 - i, 5 + 1):
+            print(j, end=" ")
+        for j in range(r - 1, r + 1 - i - 1, -1):
+            print(j, end=" ")
+        print()
+
+    print("__" * 100)
+    ########################################################################################
+    r = 5
+    for i in range(1, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        for j in range(i - 1, 1 - 1, -1):
+            print(j, end=" ")
+        print()
+
+    for i in range(r-1,1-1,-1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        for j in range(i - 1, 1 - 1, -1):
+            print(j, end=" ")
+        print()
+
+    print("__" * 100)
+    ########################################################################################
+    r = 5
+    for i in range(1,r+1):
+        for j in range(1,r-i+1):
+            print(" ",end=" ")
+        for j in range (i,1-1,-1):
+            print(j,end=" ")
+        for j in range(2,i+1):
+            print(j,end=" ")
+        print()
+
+    for i in range(r-1,1-1,-1):
+        for j in range(1,r-i+1):
+            print(" ",end=" ")
+        for j in range (i,1-1,-1):
+            print(j,end=" ")
+        for j in range(2,i+1):
+            print(j,end=" ")
+        print()
+
+    print("__" * 100)
+        ########################################################################################
+    r=5
+    for i in range(r,1-1,-1):
+            for j in range(1, r - i + 1):
+                print(" ", end=" ")
+            for j in range(5, r + 1 - i - 1, -1):
+                print(j, end=" ")
+            for j in range(r + 2 - i, r + 1):
+                print(j, end=" ")
+            print()
+
+    for i in range(2, r + 1):
+            for j in range(1, r - i + 1):
+                print(" ", end=" ")
+            for j in range(5, r + 1 - i - 1, -1):
+                print(j, end=" ")
+            for j in range(r + 2 - i, r + 1):
+                print(j, end=" ")
+            print()
+
+    print("__" * 100)
+        ########################################################################################
+
+    r = 5
+    for i in range(r, 1-1,-1):
+            for j in range(1, r - i + 1):
+                print(" ", end=" ")
+            for j in range(i, 1 - 1, -1):
+                print(j, end=" ")
+            for j in range(2, i + 1):
+                print(j, end=" ")
+            print()
+
+    for i in range(2, r + 1):
+            for j in range(1, r - i + 1):
+                print(" ", end=" ")
+            for j in range(i, 1 - 1, -1):
+                print(j, end=" ")
+            for j in range(2, i + 1):
+                print(j, end=" ")
+            print()
+    print("_"*100)
+
+#############################################################3
+    r = 5
+    for i in range(r, 1-1,-1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        for j in range(i - 1, 1 - 1, -1):
+            print(j, end=" ")
+        print()
+
+    for i in range(2, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        for j in range(i - 1, 1 - 1, -1):
+            print(j, end=" ")
+        print()
+    print("__"*100)
+    ############################################3
+    r = 5
+    for i in range(r,1-1,-1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(5, r + 1 - i - 1, -1):
+            print(j, end=" ")
+        for j in range(r + 2 - i, r + 1):
+            print(j, end=" ")
+        print()
+
+    r = 5
+    for i in range(2, r + 1):
+        for j in range(1, r - i + 1):
+            print(" ", end=" ")
+        for j in range(5, r + 1 - i - 1, -1):
+            print(j, end=" ")
+        for j in range(r + 2 - i, r + 1):
+            print(j, end=" ")
+        print()
+
+
+
+
+
