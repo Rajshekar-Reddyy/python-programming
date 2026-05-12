212dae9c75712c022f8cde753eaa1a077031eff0
Rajshekar Reddy <rajshekarreddy8050@gmail.com>
2026-05-12 22:17:36 +0530

    Implement minimum number finder in array.py
    Add program to find the minimum number in an array

---
+++ array.py
@@ -0,0 +1,9 @@
+#pgm to find min number in an array
+arr = [12,13,14,8,15,43,44,5]
+minimum=arr[0]
+for x in arr:
+    if x<minimum:
+        minimum=x
+print(minimum)
+
+######################################
