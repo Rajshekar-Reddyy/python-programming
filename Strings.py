937a94cda6a22cf094ca83455fe46e85838eeb61
Rajshekar Reddy <rajshekarreddy8050@gmail.com>
2026-07-05 21:59:51 +0530

    Refactor string reversal logic in Strings.py

--- Strings.py
+++ Strings.py
@@ -6,3 +6,12 @@ if __name__=='__main__':
         res= res+s[i]
     print(res)
 print("__"*100)
+######################################################
+#or
+if __name__=='__main__':
+    s="cockroch"
+    res=""
+    for i in range(0,len(s)):
+        res= s[i]+res
+    print(res)
+print("__"*100)
