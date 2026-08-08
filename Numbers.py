a3dd0198116bb4e0d8425e99601c392e999acd23
Rajshekar Reddy <rajshekarreddy8050@gmail.com>
2026-08-08 23:54:40 +0530

    pgm to find the smallest prime factor

--- Numbers.py
+++ Numbers.py
@@ -426,6 +426,15 @@ def fibonacci(n):
 
 fibonacci(10)
 
+################################################################3
+# Write a Python program to find the smallest prime factor of a given number.
+n = int(input("Enter a number: "))
+
+for i in range(2, n + 1):
+    if n % i == 0:
+        print("Smallest prime factor:", i)
+        break
+
 
 
 
