2fe4421df6cf8702d220f01a1b5e26f8765a2a86
Rajshekar Reddy <rajshekarreddy8050@gmail.com>
2026-08-22 23:57:11 +0530

    Add function to find first non-repeating character

--- Strings.py
+++ Strings.py
@@ -292,3 +292,12 @@ else:
 ##########################################################################################################
 #Find the first character that occurs only once
 
+text = input("Enter a string: ")
+
+for char in text:
+    if text.count(char) == 1:
+        print("First non-repeating character:", char)
+        break
+else:
+    print("No non-repeating character found")
+
