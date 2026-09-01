225c305d0ebed86ace5640b3bdda0bf74d5a5910
Rajshekar Reddy <rajshekarreddy8050@gmail.com>
2026-09-01 22:58:12 +0530

    Add divide function for sorting an array
    Implement divide function for merge sort

--- DSA.py
+++ DSA.py
@@ -24,3 +24,18 @@ def merge(arr1,arr2,res):
     return res
 
 
+def divide(arr):
+    if len(arr)==1:
+        return arr
+    mid=len(arr)//2
+    left=arr[:mid]
+    right=arr[mid:]
+    divide(left)
+    divide(right)
+    return  merge(left,right,arr)
+
+if __name__=="__main__":
+    arr=[9,7,1,5,4,2,8,3]
+    print(divide(arr))
+
+
