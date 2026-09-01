ab255d1a676020a63a0fa9adb136316c168e79c3
Rajshekar Reddy <rajshekarreddy8050@gmail.com>
2026-09-01 22:57:35 +0530

    Add merge function for merge sort algorithm
    Implemented the merge function for merge sort.

--- DSA.py
+++ DSA.py
@@ -1,1 +1,26 @@
+#MERGE SORT ALGORITHM
+
+def merge(arr1,arr2,res):
+    i=0
+    j=0
+    k=0
+    while i<len(arr1) and j<len(arr2):
+        if arr1[i]<=arr2[j]:
+            res[k]=arr1[i]
+            k=k+1
+            i=i+1
+        else:
+            res[k]=arr2[j]
+            k=k+1
+            j=j+1
+    while j<len(arr2):
+        res[k]=arr2[j]
+        k=k+1
+        j=j+1
+    while i<len(arr1):
+        res[k]=arr1[i]
+        i=i+1
+        k=k+1
+    return res
+
 
