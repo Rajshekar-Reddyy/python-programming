0a9257f788c9aafad6fc5cafc3a535a5712811b8
Rajshekar Reddy <rajshekarreddy8050@gmail.com>
2026-09-02 23:02:39 +0530

    Implement Quick Sort algorithm in DSA.py

--- DSA.py
+++ DSA.py
@@ -38,4 +38,34 @@ if __name__=="__main__":
     arr=[9,7,1,5,4,2,8,3]
     print(divide(arr))
 
+###########################################################################################################################
+# QUICK SORT
+
+def partition(arr,low,high):
+    pivot=arr[high]
+    i=low-1
+
+    for j in range(low,high):
+        if arr[j] < pivot:
+            i+=1
+            arr[i],arr[j]=arr[j],arr[i]
+
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    return i+1
+
+def quick_sort(arr,low,high):
+    if low >= high:
+        return
+    pivot_index=partition(arr,low,high)
+
+    quick_sort(arr,low,pivot_index-1)
+    quick_sort(arr,pivot_index+1,high)
+
+
+if __name__=="__main__":
+    arr=[9,7,1,5,4,2,8,3,10]
+    quick_sort(arr,0,len(arr)-1)
+    print(arr)
+
+
 
